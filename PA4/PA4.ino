007d2ef74d8c91e93700d216ae16b27a7bff7a85
maxxdmg <toorm@mail.gvsu.edu>
2019-10-02 03:23:40 -0400

    project 4 stuffs

--- PA4/PA4.ino
+++ PA4/PA4.ino
@@ -1,51 +1,78 @@
-#include <Servo.h>
 #define M_PI 3.14159265358979323846
 
-int Speed = 200;
+int Speed = 100; // ADJUST this to increase/decrease power
 int Distance = 0;
 int Angle = 0;
 
 // initialize first motor
 int pinOut1 = 6;
-int pinForward1 = 7;
-int pinBackward1 = 8;
+int pinForward1 = 8;
+int pinBackward1 = 7;
 
 // initialize second motor
 int pinOut2 = 5;
-int pinForward2 = 12;
-int pinBackward2 = 13;
+int pinForward2 = 13;
+int pinBackward2 = 12;
 
-
+int timeToSpin240 = 800; // ADJUST how long to turn the robot counter clockwise 240, spins too short -> increase, spins too long -> decrease
+int timeToSpin180 = 600; // ADJUST how long to turn the robot counter clockwise 240, spins too short -> increase, spins too long -> decrease
 
 void setup() {
   
 }
 
 void loop() {
-  setOutput(Speed);
-  rotateClockwise(120, Speed);
-  setOutput(0);
+  moveForward(1000, Speed);
+
+ /*
+  rotateCounterClockwise(timeToSpin240, Speed);  
+  delay(5);
+
+  moveForward(1000, Speed);
+  delay(5);
+
+  rotateCounterClockwise(timeToSpin240, Speed);
+  delay(5);
+
+  moveForward(1000, Speed);
+  delay(5);
+
+  rotateCounterClockwise(timeToSpin180, Speed);
+  delay(5);
+
+  moveForward(1000, Speed);
+  delay(5);
+
+  rotateCounterClockwise(timeToSpin240, Speed);
+  delay(5);
+
+  moveForward(1000, Speed);
+  delay(5);
+
+  rotateCounterClockwise(timeToSpin240, Speed);
+  delay(5);
+
+  moveForward(1000, Speed);
+  delay(5);  */
+    
   delay(10000);
 }
 
 
 // set speed output to the set speed var
 void setOutput(int spd) {
-  Serial.print("speed set to ");
-  Serial.println(spd);
-  
   // write speed output 
+  analogWrite(pinOut2, spd);  
   analogWrite(pinOut1, spd);
-  analogWrite(pinOut2, spd); 
 }
 
 
 // move robot forwards
-void moveForward(int dist, int spd) {
-  int t = spd / dist;
- 
-  Serial.println("moving forward");
-
+void moveForward(int t, int spd) { 
+  int s = 0;
+  
+  setOutput(0);
+  
   // move first motor 
   digitalWrite(pinForward1, HIGH);
   digitalWrite(pinBackward1, LOW);
@@ -54,43 +81,56 @@ void moveForward(int dist, int spd) {
   digitalWrite(pinForward2, HIGH);
   digitalWrite(pinBackward2, LOW);
 
-  delay(t);
+  // acceleration stuff here
+  while(s <= spd ) {
+        s = s + 5; // ADJUST acceleration amount
+        setOutput(s);
+        delay(50); 
+  }
+
+  delay(t - 500); // ADJUST when to begin deceleration
+
+  // deceleration stuff here
+  while(s >= 0 ) {
+        s = s - 5; // ADJUST deceleration amount
+        setOutput(s);
+        delay(50); 
+  }
+
+  setOutput(0);
 }
 
 
-// move robot backwards
-void moveBackward(int dist, int spd) {
-  int t = spd / dist;
-  
-  Serial.println("moving backward");
+// rotate robot counter clockwise 240 degrees
+void rotateCounterClockwise(int t, int spd) {
+  int s = 0;
+  int currTime = 0;
+  int prevTime = 0;
 
+  setOutput(0);
+  
   // move first motor
   digitalWrite(pinForward1, LOW);
   digitalWrite(pinBackward1, HIGH);
 
-  // move second motor
-  digitalWrite(pinForward2, LOW);
-  digitalWrite(pinBackward2, HIGH);
-
-  delay(t);
-}
-
-// rotate robot clockwise
-void rotateClockwise(int angle, int spd) {
-  int t = ((angle * 60000) / (M_PI * 63.5)) / 200;
-  
-  Serial.print("rotating clockwise by: ");
-  Serial.println(angle);
-
-  Serial.println(t);
-
-  // move first motor
-  digitalWrite(pinForward1, HIGH);
-  digitalWrite(pinBackward1, LOW);
-
   // move second motor move opposite
-  digitalWrite(pinForward2, LOW);
-  digitalWrite(pinBackward2, HIGH);
+  digitalWrite(pinForward2, HIGH);
+  digitalWrite(pinBackward2, LOW);
+
+  // acceleration stuff here
+  while(s <= spd ) {
+        s = s + 5; // ADJUST acceleration amount
+        setOutput(s);
+        delay(50); 
+  }
 
   delay(t);
+
+  // deceleration stuff here
+  while(s >= 0 ) {
+        s = s - 5; // ADJUST deceleration amount
+        setOutput(s);
+        delay(50); 
+  }
+  setOutput(0);
 }
